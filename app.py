437effde219d0532cb4185ef6fc28d8cf99204d9
rareaqil <rareaqil123@gmail.com>
2022-06-15 17:46:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,8 @@ def predict_class(sentence, model):
 
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
+    if(tag == ''):
+        result = ''
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if(i['tag'] == tag):
