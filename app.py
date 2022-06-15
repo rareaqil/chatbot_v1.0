4b20cdd7f0cec6ac6b4a817aa5509344e3e3e502
rareaqil <rareaqil123@gmail.com>
2022-06-15 17:13:48 +0700

    .

--- app.py
+++ app.py
@@ -57,8 +57,6 @@ def predict_class(sentence, model):
 
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
-    if(tag == ''):
-        result = 'Maaf kami tidak mengerti, coba ketik <b>help</b>'
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if(i['tag'] == tag):
